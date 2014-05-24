a0ac5dba0dc87278007774653cbf9f662784bb5a
alyibun <tomato_daikkirai@yahoo.co.jp>
2014-05-24 16:31:05 +0900

    hello world

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+	/*
+    for(i=0;i<10;i++){
+        printf("hello world\n");
+    }
+	*/
+	i=0;
+	while(i<10){
+		printf("hello world\n");
+		i++;
+	}
+    return 0;
+}
