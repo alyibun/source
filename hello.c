7c4e23792d3b82ac15e4f57f004e79acb1d34d66
alyibun <tomato_daikkirai@yahoo.co.jp>
2014-05-31 15:15:18 +0900

    no message

--- hello.c
+++ hello.c
@@ -8,10 +8,22 @@ int main(){
         printf("hello world\n");
     }
 	*/
+	/*
 	i=0;
 	while(i<10){
 		printf("%d hello world\n",i+1);
 		i++;
 	}
+	*/
+	i=0;
+loop:
+	if(10<=i){
+		goto ext_loop;
+	}
+	printf("i=%d Hello World\n",i+1);
+	i++;
+	goto loop;
+ext_loop:
+	
     return 0;
 }
